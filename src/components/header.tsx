e8030a0dea2333e68495e15cf1f0e984c382e259
jonassantoss <jonassoares.live@gmail.com>
2024-08-04 16:37:23 -0300

    CSS Fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,85 +1,84 @@
-import { MobileMenu } from './mobileMenu';
-import { SearchBar } from './ui/searchBar';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import * as DropdownMenu from "@radix-ui/react-dropdown-menu"
+import { MobileMenu } from "./mobileMenu";
+import { SearchBar } from "./ui/searchBar";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 
-
-import logo from '../assets/Logo.png'
-import X from '../assets/close.png';
-import menuIcon from '../assets/menu.svg'
-import searchIcon from '../assets/search.svg'
-import avatar from '../assets/Avatar.png'
+import logo from "../assets/Logo.png";
+import X from "../assets/close.png";
+import menuIcon from "../assets/menu.svg";
+import searchIcon from "../assets/search.svg";
+import avatar from "../assets/Avatar.png";
 
 export function Header() {
-    const [menuIconState, setMenuIconState] = useState(menuIcon);
-    const [searchIconState, setSearchIconState] = useState(searchIcon);
+  const [menuIconState, setMenuIconState] = useState(menuIcon);
+  const [searchIconState, setSearchIconState] = useState(searchIcon);
 
-    function handleMenuOpenChange(open: boolean) {
-        setMenuIconState(open ? X : menuIcon)
-    }
+  function handleMenuOpenChange(open: boolean) {
+    setMenuIconState(open ? X : menuIcon);
+  }
 
-    function handleSearchButtonClick() {
-        setSearchIconState(prevState => prevState === searchIcon ? X : searchIcon);
-    }
+  function handleSearchButtonClick() {
+    setSearchIconState((prevState) =>
+      prevState === searchIcon ? X : searchIcon
+    );
+  }
 
-    return (
-        <div className="w-full flex justify-between items-center gap-8">
-            <div className="relative flex items-center w-[33%]">
-                {searchIconState === X && (
-                    <SearchBar
-                        className={`absolute top-1/2 transform -translate-y-1/2 ${searchIconState === X ? 'block' : 'hidden'}`}
-                        placeholder="Busque por algo"
-                    />
-                )}
-                <Link to={"/"}>
-                    <img
-                        className={`h-1/2 ${searchIconState === X ? 'hidden' : 'block'}`}
-                        src={logo}
-                        alt=""
-                    />
-                </Link>
-            </div>
+  return (
+    <div className="w-full flex justify-between items-center gap-8">
+      <div
+        className={`relative flex items-center ${
+          searchIconState === X ? "w-3/4" : ""
+        }`}
+      >
+        {searchIconState === X && (
+          <SearchBar
+            className={`absolute top-1/2 transform -translate-y-1/2 ${
+              searchIconState === X ? "block" : "hidden"
+            }`}
+            placeholder="Busque por algo"
+          />
+        )}
+        <Link to={"/"}>
+          <img
+            className={`h-1/2 ${searchIconState === X ? "hidden" : "block"}`}
+            src={logo}
+            alt=""
+          />
+        </Link>
+      </div>
 
-            <div className="hidden md:inline-flex w-[69%]">
-                <SearchBar
-                    placeholder="Busque por algo"
-                />
-            </div>
+      <SearchBar
+        className="hidden md:inline-flex"
+        placeholder="Busque por algo"
+      />
 
-            <div className="flex justify-center">
-                <div className="md:hidden">
-                    <img
-                        className="size-11"
-                        src={searchIconState}
-                        alt=""
-                        onClick={handleSearchButtonClick}
-                    />
-                </div>
-
-                <DropdownMenu.Root onOpenChange={handleMenuOpenChange}>
-                    <DropdownMenu.Trigger
-                        asChild
-                        className="min-w-10 rounded-lg duration-100 ease-linear hover:bg-blue-300/[8%] active:bg-blue-300/15"
-                    >
-                        <img
-                            className="size-11 lg:hidden"
-                            src={menuIconState}
-                            alt=""
-                        />
-                    </DropdownMenu.Trigger>
-
-                    <MobileMenu />
-                </DropdownMenu.Root>
-            </div>
-
-            <div className="hidden lg:flex justify-end items-center gap-3 w-[33%]">
-                <img
-                    src={avatar}
-                    alt=""
-                />
-                <p>Harry</p>
-            </div>
+      <div className="flex justify-end">
+        <div className="md:hidden">
+          <img
+            className="size-11"
+            src={searchIconState}
+            alt=""
+            onClick={handleSearchButtonClick}
+          />
         </div>
-    )
+
+        <DropdownMenu.Root onOpenChange={handleMenuOpenChange}>
+          <DropdownMenu.Trigger
+            asChild
+            className="min-w-10 rounded-lg duration-100 ease-linear hover:bg-blue-300/[8%] active:bg-blue-300/15"
+          >
+            <img className="size-11 lg:hidden" src={menuIconState} alt="" />
+          </DropdownMenu.Trigger>
+
+          <MobileMenu />
+        </DropdownMenu.Root>
+      </div>
+
+      <div className="hidden lg:flex justify-end items-center gap-3">
+        <img className="size-10" src={avatar} alt="" />
+        <p className="text-lg">Harry</p>
+      </div>
+    </div>
+  );
 }
--- src/components/ui/searchBar.tsx
+++ src/components/ui/searchBar.tsx
@@ -1,14 +1,12 @@
 import { InputHTMLAttributes } from "react";
 
-interface SearchBarProps extends InputHTMLAttributes<HTMLInputElement> {
-
-}
+interface SearchBarProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 export function SearchBar(props: SearchBarProps) {
-    return (
-        <input
-            {...props}
-            className="w-full bg-slate-700 p-3 rounded-xl outline-none duration-300 ease-out hover:bg-slate-600"
-        />
-    )
+  return (
+    <input
+      {...props}
+      className="w-full bg-slate-700 p-3 rounded-xl outline-none duration-300 ease-out hover:bg-slate-600"
+    />
+  );
 }
